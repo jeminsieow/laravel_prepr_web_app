973d7cd27ea9387e3f49c748483bc106197e78c8
Sieow Je Min <sjemin7@gmail.com>
2019-06-20 22:38:42 +1000

    changed default wordings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Labs</div>
 
-                <div class="card-body">
+                <div>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="card-body">You are logged in!</div>
                 </div>
             </div>
         </div>
