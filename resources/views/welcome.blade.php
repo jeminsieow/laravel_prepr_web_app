47e9eb39156645bbe885169b5f01a59eeb97d5a6
Sieow Je Min <sjemin7@gmail.com>
2019-06-20 17:38:12 +1000

    php artisan make:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Prepr Web App
                 </div>
 
                 <div class="links">
